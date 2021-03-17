cd3a5041f4a99659e4ef8d17e8ab9fc3dba1b321
praneeth <bunnypraneeth7@gmail.com>
2021-03-17 23:00:35 +0530

    Home Page

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import {GlobalContext} from '../context/GlobalState';
+import { Link } from 'react-router-dom';
+import { List, Typography, Divider } from 'antd';
+import {
+    ListGroup,
+    ListGroupItem,
+    Button 
+} from 'reactstrap';
 
 const UserList = () => {
+    const { users } = useContext(GlobalContext)
     return (
-        <div>
-            <h1>User List</h1>
-        </div>
+        <ListGroup className="mt-4">
+            {users.map(user => (
+            <ListGroupItem className="d-flex">
+                <strong>{user.name}</strong>
+                <div className="ml-auto">
+                    <Link className="btn btn-warning mr-auto d-flex" to={`/update/${user.id}`}>Update</Link>
+                    <Button color="danger">Delete</Button>
+                </div>
+            </ListGroupItem>
+            ))}
+            
+            
+        </ListGroup>
     )
 }
 
