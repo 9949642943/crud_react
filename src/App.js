1cbb560e60b68fd9d4ec001722a07e92ea3c23b1
praneeth <bunnypraneeth7@gmail.com>
2021-03-18 01:06:30 +0530

    Done with creating, updating and deleting

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import  CreateUser  from './components/CreateUser';
 import   Home  from './components/Home';
 
 import UpdateUser from './components/UpdateUser';
+import {GlobalProvider} from './context/GlobalState';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div style={{ maxWidth: "30rem", margin: "5rem auto"}}>
-      <Router>
-        <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/create" component={CreateUser} />
-        <Route path="/update" component={UpdateUser} />
-        </Switch>
-      </Router>
+      <GlobalProvider>
+        <Router>
+          <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/create" component={CreateUser} />
+          <Route path="/update/:id" component={UpdateUser} />
+          </Switch>
+        </Router>
+      </GlobalProvider>
     </div>
   );
 }
